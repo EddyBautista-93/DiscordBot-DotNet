f0f7d5aa291aa327528fe7de6063c559539d8fdf
EddyBautista <bautista.j.eddy@gmail.com>
2021-07-12 19:38:45 -0500

    need to work on setting secrets

--- Worker.cs
+++ Worker.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -10,26 +13,48 @@ namespace DiscordBot_DotNet
 {
     public class Worker : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
+        private ILogger<Worker> logger;
+        private IConfiguration configuration;
+        private DiscordClient discordClient;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
-            _logger = logger;
+            this.logger = logger;
+            this.configuration = configuration;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        public override async Task StartAsync(CancellationToken cancellationToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
+            logger.LogInformation("Starting discord bot");
+
+            string discordBotToken = configuration["DiscordBotToken"];
+            discordClient = new DiscordClient(new DiscordConfiguration()
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                try
-                {
-                    await Task.Delay(1000, stoppingToken);
-                }
-                catch (OperationCanceledException)
-                {
-                    return;
-                }
+                Token = discordBotToken,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+
+            discordClient.MessageCreated += OnMessageCreated;
+            await discordClient.ConnectAsync();
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken) =>  Task.CompletedTask;
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            discordClient.MessageCreated -= OnMessageCreated;
+            await discordClient.DisconnectAsync();
+            discordClient.Dispose();
+            logger.LogInformation("Discord bot stopped");
+        }
+
+        private async Task OnMessageCreated(DiscordClient client, MessageCreateEventArgs e)
+        {
+            if (e.Message.Content.StartsWith("ping", StringComparison.OrdinalIgnoreCase))
+            {
+                logger.LogInformation("pinged, responding with pong!");
+                await e.Message.RespondAsync("pong!");
             }
         }
     }
